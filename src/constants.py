aedbdce622da28faad50fc5488c92a6eefedfa71
olemir <oliver.sommer42@gmail.com>
2025-04-28 16:31:55 +0200

    nobody knows what has been done here

---
+++ src/constants.py
@@ -0,0 +1,34 @@
+all_inputs = [
+    'ph', 'fixed_acidity', 'volatile_acidity', 'citric_acid',
+    'chlorides', 
+    'total_sulfur_dioxide', 'free_sulfur_dioxide', 'sulphates',
+    'residual_sugar',
+    'density',
+    'alcohol'
+    ]
+
+all_columns = [
+    'ph', 'fixed_acidity', 'volatile_acidity', 'citric_acid',
+    'chlorides', 
+    'total_sulfur_dioxide', 'free_sulfur_dioxide', 'sulphates',
+    'residual_sugar',
+    'density',
+    'alcohol',
+    'quality'
+]
+
+# plotting
+
+input_param_colors = {
+    'fixed_acidity'         : 'darkorange',
+    'volatile_acidity'      : 'burlywood',
+    'citric_acid'           : 'antiquewhite',
+    'ph'                    : 'chocolate',
+    'chlorides'             : 'turquoise',
+    'free_sulfur_dioxide'   : 'khaki',
+    'total_sulfur_dioxide'  : 'gold',
+    'sulphates'             : 'yellow',
+    'residual_sugar'        : 'crimson',
+    'density'               : 'cornflowerblue',
+    'alcohol'               : 'blueviolet'
+}
